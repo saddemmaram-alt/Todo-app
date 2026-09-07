cbb40e092af040cdae4dbb6ced69a114835421b6
maram saddem <saddemmaram@mail.com>
2026-09-07 22:23:22 +0100

    configure database connection for Neon

--- server/db.js
+++ server/db.js
@@ -1,11 +1,19 @@
 const { Pool } = require("pg");
 
-const pool = new Pool({
-  host: process.env.PGHOST || "localhost",
-  port: Number(process.env.PGPORT) || 5432,
-  database: process.env.PGDATABASE || "taskflow",
-  user: process.env.PGUSER || "postgres",
-  password: process.env.PGPASSWORD,
-});
+const databaseUrl = process.env.DATABASE_URL;
+
+const pool = databaseUrl
+  ? new Pool({
+      connectionString: databaseUrl,
+    })
+  : new Pool({
+      host: process.env.PGHOST || "localhost",
+      port: Number(process.env.PGPORT) || 5432,
+      database:
+        process.env.PGDATABASE || "taskflow",
+      user:
+        process.env.PGUSER || "postgres",
+      password: process.env.PGPASSWORD,
+    });
 
 module.exports = pool;
